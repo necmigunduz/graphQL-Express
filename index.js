815aac8bbbfea2a4800dde0105149a83bb82c8e2
necmigunduz <necmigunduz@gmail.com>
2022-01-03 15:44:58 +0300

    Add schema

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ const { graphqlHTTP } = require('express-grapql')
 const gql = require('graphql-tag')
 const { buildSchema } = require('graphql')
 
+const app = express()
+app.use(cors())
+
+const schema = buildSchema(`
+    type Query {
+        name: String
+    }
+`)
+
